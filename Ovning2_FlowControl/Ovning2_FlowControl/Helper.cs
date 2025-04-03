1d7064b57c03c7b61daae52913feb3100c044f1d
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-03 21:10:03 +0200

    feat(helper.RunnApp): add option1Starter to case 1

--- Ovning2_FlowControl/Ovning2_FlowControl/Helper.cs
+++ Ovning2_FlowControl/Ovning2_FlowControl/Helper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Option1;
+
 
 namespace Ovning2_FlowControl;
 
@@ -12,13 +14,15 @@ internal static class Helper
     {
         optionsList.Add( optionNumber, optionName );
     }*/
-
+    
     internal static void RunApp(string input)
     {
 
         switch (input)
         {
             case "0":
+                break;
+            case "1": Option1Starter.Run();
                 break;
             default: 
                 Console.WriteLine("invalid input");
