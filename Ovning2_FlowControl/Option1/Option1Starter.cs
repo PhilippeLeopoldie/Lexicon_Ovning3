5543bddcf1dcba5a9fcc8634836608460d6c3a6f
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-04 10:45:31 +0200

    feat(Option1Starter): Add total price calculation

--- Ovning2_FlowControl/Option1/Option1Starter.cs
+++ Ovning2_FlowControl/Option1/Option1Starter.cs
@@ -4,6 +4,7 @@ public static class Option1Starter
 {
     public static void Run()
     {
+        int totalPrice = 0;
         Console.Write("Enter number of person: ");
         var numberOfPerson = Option1Helper.UintValidation(Console.ReadLine());
         for(int i = 1; i <= numberOfPerson; i++)
@@ -11,8 +12,8 @@ public static class Option1Starter
             Console.Write($"Enter person {i} age: ");
             var age = Option1Helper.UintValidation(Console.ReadLine());
             Console.Write($"Person {i} has a ");
-            Option1Helper.DisplayDiscount(age);
+            totalPrice += Option1Helper.DisplayDiscount(age);
         }
-        
+        Console.WriteLine($"Total price: {totalPrice}kr");
     }
 }
