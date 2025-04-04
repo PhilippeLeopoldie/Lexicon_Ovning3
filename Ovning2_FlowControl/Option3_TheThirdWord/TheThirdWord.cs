89aa464a79f3ad21098d4d0a7b4007e2a9fe2b08
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-04 14:17:12 +0200

    feat(TheThirdWord): Extract and display the third word from user input

--- Ovning2_FlowControl/Option3_TheThirdWord/TheThirdWord.cs
+++ Ovning2_FlowControl/Option3_TheThirdWord/TheThirdWord.cs
@@ -1,11 +1,28 @@
-﻿namespace Option3_TheThirdWord
+﻿using System.Text;
+
+namespace Option3_TheThirdWord
 {
     public static class TheThirdWord
     {
         public static void Run()
         {
-            Console.WriteLine("Enter a sentence of at least 3 words:");
+            var isValid = false;
+            var result = "";
+            do
+            {
+                Console.WriteLine("Enter a sentence of at least 3 words:");
+                var input = Console.ReadLine().Trim();
+                var inputArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (inputArray.Count() >= 3)
+                {
+                    isValid = true;
+                    result = inputArray[2];
+                }
+            }
+            while (!isValid);
+            Console.WriteLine(result);
         }
+        
         
     }
 }
