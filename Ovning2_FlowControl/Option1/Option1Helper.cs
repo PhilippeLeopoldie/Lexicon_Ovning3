8a24d83cda26174d4810f40934ab18274aaf64c1
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-04 09:25:17 +0200

    feat(Option1Helper): Implement AgeValidation + DisplayDiscount

---
+++ Ovning2_FlowControl/Option1/Option1Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Option1;
+
+internal static class Option1Helper
+{
+    const int YouthPrice = 80;
+    const int SeniorPrice = 90;
+    const int StandardPrice = 120;
+
+    internal static uint AgeValidation (string input)
+    {
+        uint result;
+        while(!uint.TryParse(input, out result) || result == 0)
+        {
+            Console.WriteLine("Invalid entry");
+            Console.WriteLine("Enter your age:");
+            input = Console.ReadLine();
+        };
+        return result;
+    }
+
+    internal static int DisplayDiscount(uint age)
+    {
+        int price;
+        if(age < 20)
+        {
+            price = YouthPrice;
+            Console.WriteLine($"Youth price: {YouthPrice}kr");
+        } else if(age > 64)
+        {
+            price = SeniorPrice;
+            Console.WriteLine($"Senior price: {SeniorPrice}kr");
+        }
+        else
+        {
+            price = StandardPrice;
+            Console.WriteLine($"Standard price: {StandardPrice}kr");
+        }
+        return price;
+    }
+
+    
+}
--- Ovning2_FlowControl/Option1/Option1Starter.cs
+++ Ovning2_FlowControl/Option1/Option1Starter.cs
@@ -4,6 +4,9 @@ public static class Option1Starter
 {
     public static void Run()
     {
-        Console.WriteLine("Enter your age");
+        //Add filter: number of person then  for loop from 1 to number of person...
+        Console.Write("Enter your age: ");// change to $"Enter person {i} age "
+        var age = Option1Helper.AgeValidation(Console.ReadLine());
+        Option1Helper.DisplayDiscount(age);
     }
 }
