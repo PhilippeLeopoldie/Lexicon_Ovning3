09b6b0b8bb3bb3cafaf19cb6f06a063612635ce7
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-07 00:47:24 +0200

    refactor(RepeatTenTimes): Move private methods to Helper class

---
+++ Ovning2_FlowControl/Option2_RepeatTenTimes/RepeatTenTimes.Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace Option2_RepeatTenTimes
+{
+    internal class Helper
+    {
+        internal static void DisplayPresentation()
+        {
+            Console.WriteLine("------Repeat Ten Times------");
+            Console.Write("Enter your text: ");
+        }
+
+        internal static void DisplayNTime(string input, int numberOfRepetition)
+        {
+            for (int i = 1; i <= numberOfRepetition; i++)
+            {
+                Console.Write($"{i}. {input}");
+                if (i < numberOfRepetition) Console.Write(", ");
+            }
+        }
+    }
+}
--- Ovning2_FlowControl/Option2_RepeatTenTimes/RepeatTenTimes.cs
+++ Ovning2_FlowControl/Option2_RepeatTenTimes/RepeatTenTimes.cs
@@ -3,16 +3,27 @@ namespace Option2_RepeatTenTimes;
 
 public static class RepeatTenTimes
 {
-    const int numberOfRepetition = 10;
+    private const int numberOfRepetition = 10;
     public static void Run()
+    {
+        Helper.DisplayPresentation();
+        var input = Util.StringValidation(Console.ReadLine());
+        Helper.DisplayNTime(input,numberOfRepetition);
+        Console.WriteLine();
+    }
+
+    /*private static void DisplayPresentation()
     {
         Console.WriteLine("------Repeat Ten Times------");
         Console.Write("Enter your text: ");
-        var input = Util.StringValidation(Console.ReadLine());
+    }
+
+    private static void DisplayNTime(string input,  int numberOfRepetition)
+    {
         for (int i = 1; i <= numberOfRepetition; i++)
         {
-            Console.Write($"{i}.{input}, ");
+            Console.Write($"{i}. {input}");
+            if (i < numberOfRepetition) Console.Write(", ");
         }
-        Console.WriteLine();
-    }
+    }*/
 }
