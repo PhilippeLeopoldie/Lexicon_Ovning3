618d6fed0796780b77caf2fcb1fac41bbcd247e9
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-07 00:50:29 +0200

    refactor(RepeatTenTimes): Remove commented-out code

--- Ovning2_FlowControl/Option2_RepeatTenTimes/RepeatTenTimes.cs
+++ Ovning2_FlowControl/Option2_RepeatTenTimes/RepeatTenTimes.cs
@@ -11,19 +11,4 @@ public static class RepeatTenTimes
         Helper.DisplayNTime(input,numberOfRepetition);
         Console.WriteLine();
     }
-
-    /*private static void DisplayPresentation()
-    {
-        Console.WriteLine("------Repeat Ten Times------");
-        Console.Write("Enter your text: ");
-    }
-
-    private static void DisplayNTime(string input,  int numberOfRepetition)
-    {
-        for (int i = 1; i <= numberOfRepetition; i++)
-        {
-            Console.Write($"{i}. {input}");
-            if (i < numberOfRepetition) Console.Write(", ");
-        }
-    }*/
 }
