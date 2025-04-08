05fb5d5eb1fc64b4d2b9a7ccfe978e68e32b8221
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-08 10:14:38 +0200

    feat(OOP1): Implement OOP1 lecture exercise

---
+++ OOP2025VT/OOP12025VT/OOP12025VT/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace OOP12025VT;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Animal animal = new Animal(10.0);
+        Cat cat = new Cat();
+        Dog dog = new Dog(10.0);
+
+        /*List<Animal> animals = new List<Animal>();
+        animals.Add(dog);
+        animals.Add(cat);*/
+
+        var animals = new List<Animal>{cat, dog};
+        foreach (Animal animal in animals)
+        {
+            var lunch = new Dog(1);
+            animal.Eat(lunch);
+            Console.WriteLine(animal);
+        }
+        
+    }
+}
+// camelCase
+// PascalCase
+
+abstract class Animal
+{
+    public double Weight { get; set; }
+
+    public Animal(double weight)
+    {
+        Weight = weight;
+        MakeSound();
+    }
+    public virtual void Eat(Animal lunch)
+    {
+        Weight += lunch.Weight;
+    }
+
+    public override string ToString()
+    {
+        return $"Weight: {Weight}";
+    }
+
+    public abstract void MakeSound();
+    
+}
+
+class Cat: Animal
+{
+    public double PurrVolume { get; }
+    public Cat() : base(4.5)
+    {
+        PurrVolume = Weight * 0.5;
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, PurrVolume: {PurrVolume}";
+    }
+}
+
+class Dog: Animal 
+{
+    public Dog(double weight) : base(weight)
+    {
+
+    }
+
+    public override void Eat(Animal lunch)
+    {
+        Weight += lunch.Weight * 0.8;
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woff!");
+    }
+}
