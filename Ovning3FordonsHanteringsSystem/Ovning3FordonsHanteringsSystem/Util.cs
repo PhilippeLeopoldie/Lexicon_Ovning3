e89fc09e7b8f899c21a3037f6b082eeedfa8f9b3
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-09 11:09:25 +0200

    feat(Util): Implement utilities class for type validation

---
+++ Ovning3FordonsHanteringsSystem/Ovning3FordonsHanteringsSystem/Util.cs
@@ -0,0 +1,36 @@
+﻿namespace Option1_YouthOrSenior;
+
+public static class Util
+{
+    public static uint UintValidation(string input)
+    {
+        uint result;
+        while (!uint.TryParse(input, out result))
+        {
+            Console.Write("Invalid entry, try again: ");
+            input = Console.ReadLine();
+        };
+        return result;
+    }
+
+    public static string StringValidation(string input)
+    {
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.Write("Invalid entry, try again: ");
+            input = Console.ReadLine();
+        };
+        return input;
+    }
+
+    public static double DoubleValidation(string input)
+    {
+        double result;
+        while (!double.TryParse(input, out result))
+        {
+            Console.WriteLine("Invalid entry, try again: ");
+            input = Console.ReadLine();
+        }
+        return result;
+    }
+}
