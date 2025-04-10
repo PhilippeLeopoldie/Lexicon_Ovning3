22a87f6fd7f47b197b3a2954ef78b4ea8f311fa3
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-10 22:51:40 +0200

    refactor(Program): Reorganise structure for better clarity

--- Ovning3FordonsHanteringsSystem/Ovning3FordonsHanteringsSystem/Program.cs
+++ Ovning3FordonsHanteringsSystem/Ovning3FordonsHanteringsSystem/Program.cs
@@ -10,16 +10,32 @@ internal class Program
        
         try
         {
+            Console.WriteLine("------Del 2 Display List of SystemError objects-------");
+            List<SystemError> errorList = new List<SystemError>
+            {
+                new TransmissionError(),
+                new EngineFailureError(),
+                new BrakeFailureError()
+            };
+            foreach (SystemError error in errorList)
+            {
+                Util.Log(error.ErrorMessage());
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("-----Create 4 vehicles-------");
             VehicleHandler.CreateVehicle(new Car("Peugot", "3008", 2012, 1208, 24));
             VehicleHandler.CreateVehicle(new Motorcycle("Yamaha", "Model Yamaha", 2024, 500, true ));
             VehicleHandler.CreateVehicle(new Truck("Scania","r500", 2018, 8000, 3000));
             VehicleHandler.CreateVehicle(new ElectricScooter("Xiaomi", "m365", 2021, 12, 12.5));
             VehicleHandler.DisplayVehicles();
             Console.WriteLine();
-            Console.WriteLine("-------modify the brand of the first element----------");
+
+            Console.WriteLine("-------modify the brand of the first vehicle----------");
             VehicleHandler.UpdateBrand(VehicleHandler.ListVehicles.First(), "Volvo");
             VehicleHandler.DisplayVehicles();
             Console.WriteLine();
+
             Console.WriteLine("-------modify the vehicle 'xiamo'----------");
             VehicleHandler.UpdateVehicle(VehicleHandler.ListVehicles
                 .First(vehicle => vehicle.Brand == "xiaomi"),
@@ -27,6 +43,7 @@ internal class Program
                 );
             VehicleHandler.DisplayVehicles();
             Console.WriteLine();
+
             Console.WriteLine("----Del4: loop throw list-------");
             foreach(var vehicle in  VehicleHandler.ListVehicles)
             {
@@ -34,24 +51,11 @@ internal class Program
                 Util.Log($"{vehicle.Stats()}");
                 if(vehicle is ICleanable cleanable)
                 {
-                    cleanable.Clean();
+                    Util.Log(cleanable.Clean());
                 }
+                Console.WriteLine();
             }
             Console.WriteLine();
-            Console.WriteLine("------Del 2 error messages-------");
-            List<SystemError> errorList = new List<SystemError>
-            {
-                new TransmissionError(),
-                new EngineFailureError(),
-                new BrakeFailureError()
-            };
-
-            foreach (SystemError error in errorList)
-            {
-                Util.Log(error.ErrorMessage());
-            }
-
-
         }
         catch (Exception e)
         {
