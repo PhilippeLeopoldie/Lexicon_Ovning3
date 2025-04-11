745a2fe2d75518c69d956660eb2082b682daeab6
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-04-11 11:19:14 +0200

    feat(Program): Add answer to the exercise's questions

--- Ovning3FordonsHanteringsSystem/Ovning3FordonsHanteringsSystem/Program.cs
+++ Ovning3FordonsHanteringsSystem/Ovning3FordonsHanteringsSystem/Program.cs
@@ -6,6 +6,25 @@ using Ovning3FordonsHanteringsSystem.Vehicles;
 
 internal class Program
 {
+    /*  F: Vad händer om du försöker lägga till en Car i en lista av Motorcycle? 
+        Jag får ett kompileringsfel eftersom en lista av typen 'Motorcycle' inte 
+        kan innehålla objekt av typen 'Car', typen matchar inte. 
+      
+       
+     *  F: Vilken typ bör en lista vara för att rymma alla fordonstyper? 
+        Listan måste vara av typen 'vehicle' för att kunna rymma alla fordonstyper som äver från 'Vehicle'.
+
+
+     *  F: Kommer du åt metoden Clean() från en lista med typen List<Vehicle>? 
+        Nej, eftersom 'Vehicle' inte implementerar ICleanable, daför man måste först
+        kontrolerra om dett fordon implementerar interfacet.
+
+
+     *  F: Vad är fördelarna med att använda ett interface här istället för arv? 
+        Man kan implementera flera interfaces, medan man barra kan  arv från en basklass
+     */
+
+
     static void Main(string[] args)
     {
         try
